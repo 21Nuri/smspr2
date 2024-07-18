d00ee38ef1e6946c8ee612cb0b3a07727f391443
21Nuri <snfl7043@gmail.com>
2024-07-18 23:46:09 +0900

    Added tbpost mapper

---
+++ src/main/java/com/example/smspr2/service/TbpostService.java
@@ -0,0 +1,12 @@
+package com.example.smspr2.service;
+
+import com.example.smspr2.dto.TbpostDto;
+
+import java.util.List;
+
+public interface TbpostService {
+    TbpostDto.CreateResDto create(TbpostDto.CreateReqDto param);
+    TbpostDto.CreateResDto update(TbpostDto.UpdateReqDto param);
+    TbpostDto.SelectResDto detail(TbpostDto.SelectReqDto param);
+    List<TbpostDto.SelectResDto> list(TbpostDto.ListReqDto param);
+}
---
+++ src/main/java/com/example/smspr2/service/impl/TbpostServiceImpl.java
@@ -0,0 +1,79 @@
+package com.example.smspr2.service.impl;
+
+import com.example.smspr2.domain.Tbpost;
+import com.example.smspr2.dto.TbpostDto;
+import com.example.smspr2.mapper.TbpostMapper;
+import com.example.smspr2.repository.TbpostRepository;
+import com.example.smspr2.service.TbpostService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TbpostServiceImpl implements TbpostService {
+
+    private TbpostRepository tbpostRepository;
+    private TbpostMapper tbpostMapper;
+    public TbpostServiceImpl(
+            TbpostRepository tbpostRepository
+            ,TbpostMapper tbpostMapper
+    ) {
+        this.tbpostRepository = tbpostRepository;
+        this.tbpostMapper = tbpostMapper;
+    }
+
+    @Override
+    public TbpostDto.CreateResDto create(TbpostDto.CreateReqDto param){
+        return tbpostRepository.save(param.toEntity()).toCreateResDto();
+    }
+
+    @Override
+    public TbpostDto.CreateResDto update(TbpostDto.UpdateReqDto param){
+        Tbpost tbpost = tbpostRepository.findById(param.getId()).orElseThrow(()->new RuntimeException("no data"));
+        if(param.getTitle() != null){
+            tbpost.setTitle(param.getTitle());
+        }
+        if(param.getAuthor() != null){
+            tbpost.setAuthor(param.getAuthor());
+        }
+        if(param.getContent() != null){
+            tbpost.setContent(param.getContent());
+        }
+        tbpostRepository.save(tbpost);
+        return tbpost.toCreateResDto();
+    }
+
+    @Override
+    public TbpostDto.SelectResDto detail(TbpostDto.SelectReqDto param){
+        //1번 방법
+        /*
+        Tbpost tbpost = tbpostRepository.findById(param.getId()).orElseThrow(()->new RuntimeException("no data"));
+        TbpostDto.SelectResDto selectResDto2 = TbpostDto.SelectResDto.builder()
+                .id(tbpost.getId())
+                .createdAt(tbpost.getCreatedAt() + "")
+                .deleted(tbpost.getDeleted())
+                .title(tbpost.getTitle())
+                .author(tbpost.getAuthor())
+                .content(tbpost.getContent())
+                .build();
+        */
+
+        //2번 방법
+        TbpostDto.SelectResDto selectResDto = tbpostMapper.detail(param);
+        if(selectResDto == null){ throw new RuntimeException("no data"); }
+        return selectResDto;
+    }
+
+    @Override
+    public List<TbpostDto.SelectResDto> list(TbpostDto.ListReqDto param){
+        List<TbpostDto.SelectResDto> list = tbpostMapper.list(param);
+        //그냥 리스트만 넘기지 않고, detail 에서 상세 정보를 가져옴
+        //return list;
+        List<TbpostDto.SelectResDto> newList = new ArrayList<>();
+        for(TbpostDto.SelectResDto each : list){
+            newList.add(detail(TbpostDto.SelectReqDto.builder().id(each.getId()).build()));
+        }
+        return newList;
+    }
+}
